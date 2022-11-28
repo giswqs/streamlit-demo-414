7c75e45d11a175ea269eb3d846f144e10724e580
Qiusheng Wu <giswqs@gmail.com>
2022-11-28 09:37:14 -0500

    Added gee code

--- pages/7_❤️_Hello.py
+++ pages/7_❤️_Hello.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import geemap.foliumap as geemap
+import ee
+
 
 st.title('My Heart is with you')
+
+Map = geemap.Map(center=[21.79, 70.87], zoom=3)
+image = ee.Image('USGS/SRTMGL1_003')
+vis_params = {
+    'min': 0,
+    'max': 6000,
+    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5'],
+}
+Map.addLayer(image, vis_params, 'SRTM')
+Map.to_streamlit()
